00e8cab03289d4fc14ba47edc0b375994070cd8f
Govind-Asawa <govindasawa23@gmail.com>
2022-04-04 23:58:58 +0530

    small bug fix

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -11,11 +11,6 @@ router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/me', authController.protect, viewController.getAccount);
-router.get(
-  '/my-tours',
-  bookingController.webhookCheckout, //to create booking, set as payment's success_url
-  authController.protect,
-  viewController.getMyTours
-);
+router.get('/my-tours', authController.protect, viewController.getMyTours);
 
 module.exports = router;
